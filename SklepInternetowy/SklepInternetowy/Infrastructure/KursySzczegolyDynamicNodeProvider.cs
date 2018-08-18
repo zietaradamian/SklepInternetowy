9d09f2220c174ea4aaa31763e8f5dd20c28d14d6
zietara.sadadamzian1 <zietara.dazzzmian1@gmail.com>
2018-08-18 16:49:35 +0200

    Zainstalowanie paczki MvcSiteMapProvider i dodanie dynamicznych ścieżek nawigacyjnych

---
+++ SklepInternetowy/SklepInternetowy/Infrastructure/KursySzczegolyDynamicNodeProvider.cs
@@ -0,0 +1,32 @@
+﻿using MvcSiteMapProvider;
+using SklepInternetowy.DAL;
+using SklepInternetowy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SklepInternetowy.Infrastructure
+{
+    public class KursySzczegolyDynamicNodeProvider: DynamicNodeProviderBase
+    {
+        private KursyContext db = new KursyContext();
+        public override IEnumerable<DynamicNode> GetDynamicNodeCollection(ISiteMapNode nodee)
+        {
+            var returnValue = new List<DynamicNode>();
+
+            foreach(Kurs kurs in db.Kursy )
+            {
+                DynamicNode node = new DynamicNode();
+                node.Title = kurs.TytulKursu;
+                node.Key = "Kurs_" + kurs.KursId;
+                node.ParentKey = "Kategoria_" + kurs.KategoriaId;
+                node.RouteValues.Add("id", kurs.KursId);
+                returnValue.Add(node);
+            }
+
+
+            return returnValue;
+        }
+    }
+}
