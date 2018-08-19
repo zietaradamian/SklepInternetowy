625de16350531dafbd62917178699c9605ce33db
zietara.sadadamzian1 <zietara.dazzzmian1@gmail.com>
2018-08-19 19:05:23 +0200

    Implementacja widoku koszyka

--- SklepInternetowy/SklepInternetowy/Controllers/KoszykController.cs
+++ SklepInternetowy/SklepInternetowy/Controllers/KoszykController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using SklepInternetowy.DAL;
+using SklepInternetowy.Infrastructure;
+using SklepInternetowy.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,14 +11,54 @@ namespace SklepInternetowy.Controllers
 {
     public class KoszykController : Controller
     {
+        private KoszykMenager koszykMenager;
+        private ISessionMenager sessionMenager { get; set; }
+        private KursyContext db;
+
+        public KoszykController()
+        {
+            db = new KursyContext();
+            sessionMenager = new SessionMenager();
+            koszykMenager = new KoszykMenager(sessionMenager, db);
+        }
         // GET: Koszyk
         public ActionResult Index()
         {
-            return View();
+            var pozycjeKoszyka = koszykMenager.PobierzKoszyk();
+            var cenaCalkowita = koszykMenager.PobierzWartoscKoszyka();
+
+            KoszykViewModel koszykViewModel = new KoszykViewModel()
+            {
+                PozycjeKoszyka = pozycjeKoszyka,
+                CenaCalkowita = cenaCalkowita
+
+            };
+            return View(koszykViewModel);
         }
-        public ActionResult DodajDoKoszyka(string id)
+        public ActionResult DodajDoKoszyka(int id)
         {
-            return View();
+            koszykMenager.DodajDoKoszyka(id);
+            return RedirectToAction("Index");
+        }
+        public int PobierzIloscElementowKoszyka()
+        {
+            return koszykMenager.PobierzIloscPozycjiKoszyka();
+        }
+
+        public ActionResult UsunZKoszyka(int kursId)
+        {
+            int iloscPozycji = koszykMenager.UsunZKoszyka(kursId);
+            int iloscPozycjiKoszyka = koszykMenager.PobierzIloscPozycjiKoszyka();
+            decimal wartoscKoszyka = koszykMenager.PobierzWartoscKoszyka();
+
+            var wynik = new KoszykUsuwanieViewModel()
+            {
+                IdPozycjiUsuwanej = kursId,
+                IloscPozycjiUsuwanej = iloscPozycji,
+                KoszykCenaCalkowita = wartoscKoszyka,
+                KoszykIloscPozycji = iloscPozycjiKoszyka
+            };
+            return Json(wynik);
         }
     }
 }
---
+++ SklepInternetowy/SklepInternetowy/ViewModels/KoszykViewModel.cs
@@ -0,0 +1,14 @@
+﻿using SklepInternetowy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SklepInternetowy.ViewModels
+{
+    public class KoszykViewModel
+    {
+        public List<PozycjaKoszyka> PozycjeKoszyka { get; set; }
+        public decimal CenaCalkowita { get; set; }
+    }
+}
